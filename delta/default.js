9226553461a199d681ae82577bfc3a0a1e4efc3e
Damian <prylaris.git@gmail.com>
2018-12-17 12:36:09 -0500

    Create default.js

---
+++ delta/default.js
@@ -0,0 +1,9 @@
+moudle.exports = {
+   name: "Default",
+   location: "us-west",
+   premium: false,
+   beta: false,
+   domain: "localhost",
+   port: 3229,
+   password: "dummy u aint getting this"
+};
