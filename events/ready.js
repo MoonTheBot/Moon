cad4040095f77589a0556671296a2ca2c587230e
Damian <prylaris.git@gmail.com>
2018-12-17 12:37:47 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    client.logger.log(`Logged in as ${client.user.username} and on ${client.guilds.size} guilds.`);
+    //client.logger.log(`Logged in as ${client.user.username} and on ${client.guilds.size} guilds.`);
     client.user.setStatus("dnd");
     client.user.setActivity(`${client.config.defaultSettings.prefix}help | ${client.guilds.size} servers`);
 };
